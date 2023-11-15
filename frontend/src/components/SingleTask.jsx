2c888c83540bfc4ea133fdba1f15091905216df6
Hashir <70011762+HashMaster02@users.noreply.github.com>
2023-11-15 12:02:23 -0600

    SingleTask component can now function like a regular checklist item. Is not complete yet (needs menu, add to tasks button, etc.)

--- frontend/src/components/SingleTask.jsx
+++ frontend/src/components/SingleTask.jsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { BsExclamationCircleFill } from "react-icons/bs";
 
-function SingleTask({ group, task, dueDate, color }) {
-  // TODO: Get "color" attribute to work
+const colorPalette = {
+  red: "text-[#ff1d15]",
+  blue: "text-[#6bbaec]",
+  green: "text-[#04724d]",
+  yellow: "text-[#edae49]",
+  orange: "text-[#ffb238]",
+  purple: "text-[#54428e]",
+  pink: "text-[#ac80a0]",
+};
+
+function SingleTask({ id, group, task, dueDate, color }) {
   return (
     <li className="flex items-center">
-      <input type="checkbox" id="task1" />
+      <input type="checkbox" id={id} />
       <label
         className="leading-[1.2rem] font-shantell-sans
 						before:outline before:outline-2 before:outline-bluish-turqoise
                         "
-        htmlFor="task1"
+        htmlFor={id}
       >
-        <span className="text-[#c0d6df]">{group}</span> {task}
+        <span
+          className={` font-medium ${
+            colorPalette[color] ? colorPalette[color] : "text-black"
+          }`}
+        >
+          {group}
+        </span>{" "}
+        {task}
         <small className="block font-light text-bluish-turqoise text-xs">
           <BsExclamationCircleFill className="inline-block mr-1 text-red-600" />
           Due {dueDate}
--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -2,16 +2,52 @@ import Header from "../components/Header";
 import SingleTask from "../components/SingleTask";
 import { FaListCheck } from "react-icons/fa6";
 
+const tasksList = [
+  {
+    _id: 1,
+    group: "MATH-332",
+    todo: "Written Assignment 12",
+    dueDate: "25th Oct 2023",
+    color: "purple",
+  },
+  {
+    _id: 2,
+    group: "CS-350",
+    todo: "MP1",
+    dueDate: "16th Oct 2023",
+    color: "red",
+  },
+  {
+    _id: 3,
+    group: "ITMD-361",
+    todo: "Lab 6",
+    dueDate: "30th Oct 2023",
+    color: "yellow",
+  },
+  {
+    _id: 4,
+    group: "ITMD-362",
+    todo: "Lab 1",
+    dueDate: "10th Nov 2023",
+    color: "pink",
+  },
+];
+
 function Tasks() {
   return (
     <>
       <Header title={"Tasks"} icon={<FaListCheck />} showDate />
       <ul className="m-6 flex flex-col gap-4 ">
-        <SingleTask
-          group="MATH-332"
-          task="Written Assignment 12"
-          dueDate="25th Oct 2023"
-        />
+        {tasksList.map((task) => (
+          <SingleTask
+            key={task._id}
+            id={task._id}
+            group={task.group}
+            task={task.todo}
+            dueDate={task.dueDate}
+            color={task.color}
+          />
+        ))}
       </ul>
     </>
   );
