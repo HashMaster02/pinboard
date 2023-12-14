7980b33ab4150d762f22947c129d6be479895161
Hash <70011762+HashMaster02@users.noreply.github.com>
2023-12-14 00:54:25 -0600

    Tasks now show warning only when due date is within 2 days

--- frontend/src/components/SingleTask.jsx
+++ frontend/src/components/SingleTask.jsx
@@ -1,42 +1,48 @@
-import { BsExclamationCircleFill } from "react-icons/bs";
+import { BsExclamationCircleFill } from 'react-icons/bs';
 
 const colorPalette = {
-  red: "text-[#ff1d15]",
-  blue: "text-[#6bbaec]",
-  green: "text-[#04724d]",
-  yellow: "text-[#edae49]",
-  orange: "text-[#ffb238]",
-  purple: "text-[#54428e]",
-  pink: "text-[#ac80a0]",
+    red: 'text-[#ff1d15]',
+    blue: 'text-[#6bbaec]',
+    green: 'text-[#04724d]',
+    yellow: 'text-[#edae49]',
+    orange: 'text-[#ffb238]',
+    purple: 'text-[#54428e]',
+    pink: 'text-[#ac80a0]',
 };
 
-// TODO: Add date logic (show warning when due date less than 2 days away; )
-
 function SingleTask({ id, group, task, dueDate, color }) {
-  return (
-    <li id="task-item" className="flex items-center">
-      <input type="checkbox" id={id} />
-      <label
-        className="leading-[1.2rem] font-shantell-sans
+    return (
+        <li id="task-item" className="flex items-center">
+            <input type="checkbox" id={id} />
+            <label
+                className="leading-[1.2rem] font-shantell-sans
 						before:outline before:outline-2 before:outline-bluish-turqoise
                         "
-        htmlFor={id}
-      >
-        <span
-          className={` font-medium ${
-            colorPalette[color] ? colorPalette[color] : "text-black"
-          }`}
-        >
-          {group}
-        </span>{" "}
-        {task}
-        <small className="block font-light text-bluish-turqoise text-xs">
-          <BsExclamationCircleFill className="inline-block mr-1 text-red-600" />
-          Due {dueDate}
-        </small>
-      </label>
-    </li>
-  );
+                htmlFor={id}
+            >
+                <span
+                    className={` font-medium ${
+                        colorPalette[color] ? colorPalette[color] : 'text-black'
+                    }`}
+                >
+                    {group}
+                </span>{' '}
+                {task}
+                <small className="block font-light text-bluish-turqoise text-xs">
+                    {daysToDueDate(dueDate) <= 2 && (
+                        <BsExclamationCircleFill className="inline-block mr-1 text-red-600" />
+                    )}
+                    Due {dueDate.toDateString()}
+                </small>
+            </label>
+        </li>
+    );
+}
+
+function daysToDueDate(due) {
+    // Calculates the number of days between today and date given
+    const today = new Date();
+    return Math.abs(today.getTime() - due.getTime()) / (1000 * 60 * 60 * 24.0);
 }
 
 export default SingleTask;
--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,66 +1,59 @@
-import { useEffect } from "react";
-import Header from "../components/Header";
-import SingleTask from "../components/SingleTask";
-import { FaListCheck } from "react-icons/fa6";
+import { useEffect } from 'react';
+import Header from '../components/Header';
+import SingleTask from '../components/SingleTask';
+import { FaListCheck } from 'react-icons/fa6';
 
 const tasksList = [
-  {
-    _id: 1,
-    group: "MATH-332",
-    todo: "Written Assignment 12",
-    dueDate: "Oct 13, 2023 22:00:00",
-    color: "purple",
-  },
-  {
-    _id: 2,
-    group: "CS-350",
-    todo: "MP1",
-    dueDate: "Oct 16, 2023 22:00:00",
-    color: "red",
-  },
-  {
-    _id: 3,
-    group: "ITMD-361",
-    todo: "Lab 6",
-    dueDate: "Oct 30, 2023 22:00:00",
-    color: "yellow",
-  },
-  {
-    _id: 4,
-    group: "ITMD-362",
-    todo: "Lab 1",
-    dueDate: "Nov 10, 2023 22:00:00",
-    color: "pink",
-  },
+    {
+        _id: 1,
+        group: 'MATH-332',
+        todo: 'Written Assignment 12',
+        dueDate: '2023-12-16',
+        color: 'purple',
+    },
+    {
+        _id: 2,
+        group: 'CS-350',
+        todo: 'MP1',
+        dueDate: '2023-10-18',
+        color: 'red',
+    },
+    {
+        _id: 3,
+        group: 'ITMD-361',
+        todo: 'Lab 6',
+        dueDate: '2023-10-01',
+        color: 'yellow',
+    },
+    {
+        _id: 4,
+        group: 'ITMD-362',
+        todo: 'Lab 1',
+        dueDate: '2023-05-24',
+        color: 'pink',
+    },
 ];
 
 // TODO: Make delete button via a context menu
 
 function Tasks() {
-  let dateObj;
-  useEffect(() => {
-    dateObj = document.getElementById("date-test");
-    console.log(dateObj.value);
-  });
-
-  return (
-    <>
-      <input type="date" id="date-test" value="2017-06-01" />
-      <Header title={"Tasks"} icon={<FaListCheck />} showDate />
-      <ul className="m-6 flex flex-col gap-4 ">
-        {tasksList.map((task) => (
-          <SingleTask
-            key={task._id}
-            id={task._id}
-            group={task.group}
-            task={task.todo}
-            dueDate={new Date("06/01/2017").toString()}
-            color={task.color}
-          />
-        ))}
-      </ul>
-    </>
-  );
+    return (
+        <>
+            <Header title={'Tasks'} icon={<FaListCheck />} showDate />
+            <ul className="m-6 flex flex-col gap-4 ">
+                {tasksList.map((task) => (
+                    <SingleTask
+                        key={task._id}
+                        id={task._id}
+                        group={task.group}
+                        task={task.todo}
+                        dueDate={new Date(task.dueDate.split('-'))}
+                        color={task.color}
+                    />
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default Tasks;
