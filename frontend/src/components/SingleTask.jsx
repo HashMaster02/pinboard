78e3ba5aa45bc1bbca42906f6894cec357a3fbac
Hashir <70011762+HashMaster02@users.noreply.github.com>
2024-02-02 12:52:56 -0600

    Fixed bug to ensure red icon only shows after due date on task object

--- frontend/src/components/SingleTask.jsx
+++ frontend/src/components/SingleTask.jsx
@@ -1,9 +1,9 @@
-import { useContext, useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { BsExclamationCircleFill } from 'react-icons/bs';
-import { FaEllipsis, FaTriangleExclamation } from 'react-icons/fa6';
-import TasksContext from '../context/TasksContext';
-import PopupMenu from './PopupMenu';
+import { useContext, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { BsExclamationCircleFill } from "react-icons/bs";
+import { FaEllipsis, FaTriangleExclamation } from "react-icons/fa6";
+import TasksContext from "../context/TasksContext";
+import PopupMenu from "./PopupMenu";
 
 function SingleTask({ id, group, task, dueDate, color, checked }) {
     const { toggleCheck, daysToDueDate } = useContext(TasksContext);
@@ -13,14 +13,14 @@ function SingleTask({ id, group, task, dueDate, color, checked }) {
 
     useEffect(() => {
         if (checked) {
-            document.getElementById(`task-${id}`).style.color = '#e1e5f2';
+            document.getElementById(`task-${id}`).style.color = "#e1e5f2";
             return;
         }
         document.getElementById(`task-${id}`).style.color = color;
     }, [checked]);
 
-    document.addEventListener('click', (e) => {
-        if (showMenu && e.target.id !== 'menu-button') {
+    document.addEventListener("click", (e) => {
+        if (showMenu && e.target.id !== "menu-button") {
             setShowMenu(false);
         }
     });
@@ -35,7 +35,7 @@ function SingleTask({ id, group, task, dueDate, color, checked }) {
     return (
         <div className="flex justify-between">
             <li id="task-item" className="flex items-center">
-                {pathname === '/tasks' && (
+                {pathname === "/tasks" && (
                     <input
                         onChange={() => toggleCheck(id)}
                         type="checkbox"
@@ -45,20 +45,20 @@ function SingleTask({ id, group, task, dueDate, color, checked }) {
                 )}
                 <label
                     className={
-                        pathname === '/tasks'
-                            ? 'leading-[1.2rem] font-shantell-sans before:outline before:outline-2 before:outline-bluish-turqoise'
-                            : 'leading-[1.2rem] font-shantell-sans'
+                        pathname === "/tasks"
+                            ? "leading-[1.2rem] font-shantell-sans before:outline before:outline-2 before:outline-bluish-turqoise"
+                            : "leading-[1.2rem] font-shantell-sans"
                     }
                     htmlFor={id}
                 >
                     <span id={`task-${id}`} className="font-medium uppercase">
                         {group}
-                    </span>{' '}
+                    </span>{" "}
                     {task}
                     <small className="block font-light text-bluish-turqoise text-xs">
                         {daysLeft > 0 && daysLeft <= 2 ? (
                             <FaTriangleExclamation className="inline-block mr-1 text-yellow-400" />
-                        ) : daysLeft <= 0 ? (
+                        ) : daysLeft < 0 ? (
                             <BsExclamationCircleFill className="inline-block mr-1 text-red-500" />
                         ) : null}
                         Due {dueDate.toDateString()}
